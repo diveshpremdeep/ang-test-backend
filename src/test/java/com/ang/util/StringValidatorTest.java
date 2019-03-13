71b92f0fc16edf17f7ed21ba5a9acc93ec864e3a
Divesh Premdeep <diveshpremdeep@gmail.com>
2019-03-13 12:29:26 +1000

    Create the validator as part of the setup

--- src/test/java/com/ang/util/StringValidatorTest.java
+++ src/test/java/com/ang/util/StringValidatorTest.java
@@ -1,5 +1,6 @@
 package com.ang.util;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
@@ -7,9 +8,15 @@ import static org.junit.Assert.assertTrue;
 
 public class StringValidatorTest {
 
+    private StringValidator validator;
+
+    @Before
+    public void setUp() {
+        this.validator = new StringValidator();
+    }
+
     @Test
     public void testValidateBracketsWithValidinput() {
-        final StringValidator validator = new StringValidator();
         assertTrue(validator.validateBrackets("a"));
         assertTrue(validator.validateBrackets("abcd efgh"));
         assertTrue(validator.validateBrackets("[]"));
@@ -35,22 +42,16 @@ public class StringValidatorTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void testValidateBracketsWithNullInput() {
-        final StringValidator validator = new StringValidator();
-
         validator.validateBrackets(null);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testValidateBracketsWithEmptyInput() {
-        final StringValidator validator = new StringValidator();
-
         validator.validateBrackets("");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testValidateBracketsWithGiganticInput() {
-        final StringValidator validator = new StringValidator();
-
         // Use a string having length > 100 chars.
         // Could probably use a string generator here; keeping things simple for now.
         final String alphabet = "abcdefghijklmnopqrstuvwxyz";
