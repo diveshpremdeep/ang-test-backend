8a1c10cfb8c92fd9f7422f938ece63b19adaea87
Divesh Premdeep <diveshpremdeep@gmail.com>
2019-03-14 16:22:29 +1000

    Format the createdDate.

--- src/main/java/com/ang/model/ToDoItem.java
+++ src/main/java/com/ang/model/ToDoItem.java
@@ -1,10 +1,13 @@
 package com.ang.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
 import java.util.Date;
+
+import static com.fasterxml.jackson.annotation.JsonFormat.Shape.STRING;
 
 @Data
 @Builder
@@ -16,6 +19,7 @@ public class ToDoItem {
     @JsonProperty("isCompleted")
     private final boolean completed;
 
+    @JsonFormat(shape = STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
     private final Date createdAt;
 
 }
