4b11d89b2310f3ab87c6e9b7af444e6f46475f0c
Divesh Premdeep <diveshpremdeep@gmail.com>
2019-03-14 21:38:04 +1000

    Add an endpoint for patching todo items

--- src/main/java/com/ang/controller/ToDoController.java
+++ src/main/java/com/ang/controller/ToDoController.java
@@ -3,6 +3,7 @@ package com.ang.controller;
 import com.ang.controller.exception.ToDoItemNotFoundException;
 import com.ang.model.ToDoItem;
 import com.ang.model.ToDoItemAddRequest;
+import com.ang.model.ToDoItemUpdateRequest;
 import com.ang.service.ToDoService;
 import com.ang.util.exception.InvalidInputException;
 import com.google.common.base.Preconditions;
@@ -32,13 +33,7 @@ public class ToDoController {
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<ToDoItem> getToDoItem(@PathVariable("id") String idStr) {
-        final int idInt = Optional.ofNullable(idStr)
-            .map(String::trim)
-            .flatMap(this::parseIntOpt)
-            .orElseThrow(() -> new InvalidInputException(
-                idStr,
-                "The ID should be a valid 32-bit number"
-            ));
+        final int idInt = validateAndNumerifyId(idStr);
 
         final ToDoItem item = toDoService.getToDoItem(idInt)
             .orElseThrow(() -> new ToDoItemNotFoundException(idInt));
@@ -46,6 +41,29 @@ public class ToDoController {
         return ResponseEntity.ok()
             .contentType(MediaType.APPLICATION_JSON)
             .body(item);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PATCH)
+    public ResponseEntity<ToDoItem> updateToDoItem(@PathVariable("id") String idStr,
+                                                   @RequestBody ToDoItemUpdateRequest updateReq) {
+        final int idInt = validateAndNumerifyId(idStr);
+
+        final ToDoItem item = toDoService.updateToDoItem(idInt, updateReq)
+            .orElseThrow(() -> new ToDoItemNotFoundException(idInt));
+
+        return ResponseEntity.ok()
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(item);
+    }
+
+    private int validateAndNumerifyId(String idStr) {
+        return Optional.ofNullable(idStr)
+            .map(String::trim)
+            .flatMap(this::parseIntOpt)
+            .orElseThrow(() -> new InvalidInputException(
+                idStr,
+                "The ID should be a valid 32-bit number"
+            ));
     }
 
     // Very hacky, but keeping it simple for now.
